de02431735d6f2e39e849676a7d55b3a2609711b
swapon789 <kumarshopon850@gmail.com>
2022-11-10 19:21:14 +0600

    get service section on mongo user

--- index.js
+++ index.js
@@ -12,6 +12,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('dentrexaUser').collection('services');
+      
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+        app.get('/allservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+        app.post('/services', async (req, res) => {
+            const order = req.body;
+            const result = await serviceCollection.insertOne(order);
+            res.send(result);
+        });
+        
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
+
 app.get('/', (req, res) => {
     res.send("Dentrexa server on running")
 });
