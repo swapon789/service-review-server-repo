d5039799fed3da4980136bb95544d90283a9b7d9
swapon789 <kumarshopon850@gmail.com>
2022-11-10 19:58:11 +0600

    Delete section added

--- index.js
+++ index.js
@@ -86,6 +86,14 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result)
+
+        })
+
     }
     finally {
 
